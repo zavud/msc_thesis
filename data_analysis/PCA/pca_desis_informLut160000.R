0c2ef56209fc7345f976d03cae241c8349c0e085
zavud <zavudb@gmail.com>
2021-07-03 14:01:57 +0200

    pca on desis LUT (size of 160000) finished

---
+++ data_analysis/PCA/pca_desis_informLut160000.R
@@ -0,0 +1,60 @@
+# load libraries
+library(tidymodels)
+library(tidyverse)
+library(raster)
+library(rgdal)
+
+# load prisma wl bands
+wl_path = "C:\\Users\\zavud\\Desktop\\msc_thesis\\data_analysis\\rtm_inform\\sensor_metadata\\wl_desis.txt"
+wl = readr::read_csv(wl_path, col_names = F)[[1]]
+nms = c(wl, "Cab", "Cm", "LAI", "cd")
+
+# load the lut data
+lut_path = "C:\\Users\\zavud\\Desktop\\msc_thesis\\data_analysis\\rtm_inform\\LUT_databases"
+lut_files = list.files(path = lut_path, pattern = "160000", full.names = T)
+lut = read_csv(lut_files) %>% setNames(nms)
+
+# shuffle the data
+lut = lut %>% slice(sample(1:n()))
+
+# divide the data into train/val/test sets
+data_split = initial_split(lut, prop = .75)
+training_val = training(data_split)
+training = training_val[1:100000,]
+validation = training_val[-c(1:100000), ]
+testing = testing(data_split)
+rm(training_val)
+
+# build a PCA model
+pca_recipe = recipe(training, ~.) %>% 
+        update_role(236:239, new_role = "id") %>% 
+        step_normalize(all_predictors()) %>% 
+        step_pca(all_predictors(), num_comp = 6)
+pca_prep = pca_recipe %>% prep()
+pca_tidy = tidy(pca_prep, 2)
+
+# variations explained
+sdev = pca_prep$steps[[2]]$res$sdev
+percent_variation = sdev^2 / sum(sdev^2)
+tibble(component = unique(pca_tidy$component)[1:6],
+       variation = percent_variation[1:6]) %>% 
+        ggplot(aes(x = component, y = variation, fill = component)) +
+        geom_col(show.legend = F) +
+        scale_y_continuous(labels = scales::percent_format(), limits = c(0, 1)) +
+        labs(title = "Variation explained by the first 6 PCs",
+             subtitle = expression("Cumulative variation of 6 PCs " %~~% "100%"),
+             x = "Principal Component",
+             y = "Variation explained") +
+        theme_bw() +
+        theme(plot.title = element_text(hjust = .5),
+              plot.subtitle = element_text(hjust = .5))
+
+# apply the transformation to validation and test sets
+pca_training = bake(pca_prep, new_data = NULL)
+pca_validation = bake(pca_prep, new_data = validation)
+pca_testing = bake(pca_prep, new_data = testing)
+
+# save the PCA data sets
+write_csv(pca_training, file = "C:\\Users\\zavud\\Desktop\\msc_thesis\\data_analysis\\desis_training_database\\pca_training.txt")
+write_csv(pca_validation, file = "C:\\Users\\zavud\\Desktop\\msc_thesis\\data_analysis\\desis_training_database\\pca_validation.txt")
+write_csv(pca_testing, file = "C:\\Users\\zavud\\Desktop\\msc_thesis\\data_analysis\\desis_training_database\\pca_testing.txt")
