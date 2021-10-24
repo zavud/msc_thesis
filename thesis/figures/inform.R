b25025d61c0043448bf46ded988c19c50b7eea7d
Zavud Baghirov <hi63tov@ads.uni-jena.de>
2021-10-24 15:58:45 +0200

    title page, r code chunks (using .//) updated

--- thesis/figures/inform.R
+++ thesis/figures/inform.R
@@ -7,25 +7,29 @@ node [shape = rectangle, style = filled, fillcolor = Linen, fontname = Helvetica
 
 # prospect5
 leafp [label = 'Leaf \n parameters', shape = folder, fillcolor = Beige]
-p5 [label = 'PROSPECT5']
+p5 [label = 'PROSPECT5', fillcolor = Pink]
 leaf_ref [label = 'Leaf \n reflectance', shape = cylinder]
 leaf_tr [label = 'Leaf \n transmittance', shape = cylinder]
 
 # 4sail
-canopyp [label = 'Canopy & Geometric \n parameters', shape = folder, fillcolor = Beige]
-foursail [label = '4SAIL']
+canopyp [label = 'Canopy \n parameters', shape = folder, fillcolor = Beige]
+geop [label = 'Geometric \n parameters', shape = folder, fillcolor = Beige]
+bg [label = 'Backround \n reflectance', shape = folder, fillcolor = Beige]
+foursail [label = '4SAIL', fillcolor = Pink]
 
 to [label = 'Transmittance \n in viewing direction', shape = cylinder]
 ts [label = 'Transmittance \n in sun direction', shape = cylinder]
 
 # flim
-canopyp2 [label = 'Canopy & Geometric \n parameters', shape = folder, fillcolor = Beige]
-flim [label = 'FLIM']
+canopyp2 [label = 'Canopy \n parameters', shape = folder, fillcolor = Beige]
+geop2 [label = 'Geometric \n parameters', shape = folder, fillcolor = Beige]
+bg2 [label = 'Backround \n reflectance', shape = folder, fillcolor = Beige]
+flim [label = 'FLIM', fillcolor = Pink]
 simref [label = 'Canopy \n reflectance', shape = cylinder]
 
 # inform
 leafp -> p5 -> {leaf_ref leaf_tr} -> foursail -> {to ts} -> flim -> simref
-canopyp -> foursail
-canopyp2 -> flim
+{canopyp geop bg} -> foursail
+{canopyp2 geop2 bg2} -> flim
                   
 }", height="100%", width="100%")
--- thesis/figures/prosail.R
+++ thesis/figures/prosail.R
@@ -7,17 +7,19 @@ node [shape = rectangle, style = filled, fillcolor = Linen, fontname = Helvetica
 
 # prospect5
 leafp [label = 'Leaf \n parameters', shape = folder, fillcolor = Beige]
-p5 [label = 'PROSPECT5']
+p5 [label = 'PROSPECT5', fillcolor = Pink]
 leaf_ref [label = 'Leaf \n reflectance', shape = cylinder]
 leaf_tr [label = 'Leaf \n transmittance', shape = cylinder]
 
 # 4sail
-canopyp [label = 'Canopy & Geometric \n parameters', shape = folder, fillcolor = Beige]
-foursail [label = '4SAIL']
+canopyp [label = 'Canopy \n parameters', shape = folder, fillcolor = Beige]
+geop [label = 'Geometric \n parameters', shape = folder, fillcolor = Beige]
+bg [label = 'Backround \n reflectance', shape = folder, fillcolor = Beige]
+foursail [label = '4SAIL', fillcolor = Pink]
 simref [label = 'Canopy \n reflectance', shape = cylinder]
 
 #prosail
 leafp -> p5 -> {leaf_ref leaf_tr} -> foursail -> simref
-canopyp -> foursail                  
+{canopyp geop bg} -> foursail                  
                   
 }", height="100%", width="100%")
